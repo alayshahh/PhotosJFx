a6571d58e47433fbabe1e0f60cd8de6eaa7079bf
Alay Shah <alayshah9@icloud.com>
2020-11-15 16:06:55 -0500

    Created User Object

---
+++ src/model/User.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.io.Serializable;
+import java.util.*;
+/**
+ * 
+ * @author Alay Shah
+ * @author Anshika Khare
+ *
+ */
+
+public class User implements Serializable{
+	
+	static final long serialVersionUID = 1L;
+	ArrayList<Album> albums = new ArrayList<>();
+	private String userName;
+	
+	
+	
+}
