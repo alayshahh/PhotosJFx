d07adaf299e3eca2fa31737f3456d39e14c4501e
Alay Shah <alayshah9@icloud.com>
2020-11-16 16:00:09 -0500

    added Comments and userList Object field

--- src/model/UserList.java
+++ src/model/UserList.java
@@ -10,7 +10,8 @@ import java.util.ArrayList;
  */
 
 public class UserList implements Serializable {
-
+	
+	static UserList userList = new UserList();
 	
 	static final long serialVersionUID = 1L;
 	public static final String storDir = "dat";
@@ -19,18 +20,83 @@ public class UserList implements Serializable {
 	
 	ArrayList<User> allUsers = new ArrayList<>();
 	
+	/**
+	 * Takes in User and add it to the list
+	 * @param me
+	 */
 	public void addUser(User me) {
 		allUsers.add(me);
 	}
 	
+	/**
+	 * Removes given user from the list
+	 * @param me
+	 */
 	public void removeUser(User me) {
 		allUsers.remove(me);
 	}
 	
+	/**
+	 * Tells if the current user is in the list
+	 * @param me
+	 * @return
+	 */
 	public boolean contains(User me) {
 		return allUsers.contains(me);
 	}
 	
-
+	/**
+	 * Tells if a user with the current username is in the list
+	 * @param userName
+	 * @return
+	 */
+	public boolean contains(String userName) {
+		for(User u: allUsers) {
+			if(userName.equals(u.getUserName())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	/**
+	 * Tells if userList is empty
+	 * @return
+	 */
+	public boolean isEmpty() {
+		return allUsers.isEmpty();
+	}
+	
+	/**
+	 * returns the user based on the given Username
+	 * @param userName
+	 * @return
+	 */
+	public User getUser(String userName) {
+		for(User u: allUsers) {
+			if(u.getUserName().equals(userName)) {
+				return u;
+			}
+		}
+		return null;
+	}
+	
+	
+	public void writeApp() throws IOException{
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(storDir + File.separator + storeFile));
+		oos.writeObject(userList);
+		oos.close();
+	}
+	
+	public void readApp() throws IOException, ClassNotFoundException{
+		ObjectInputStream  ois = new ObjectInputStream( new FileInputStream(storDir+File.separator+storeFile));
+		userList = (UserList) ois.readObject();
+		ois.close();
+		
+	}
+	public static UserList getUserList() {
+		return userList;
+	}
 }
 
