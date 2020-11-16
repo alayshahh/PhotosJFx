423878a9608f57cdb04b09079bc62a089c99f8cf
Alay Shah <alayshah9@icloud.com>
2020-11-15 19:08:15 -0500

    getters, setters,hasAlbum, addAlbum, delete Album methods

--- src/model/User.java
+++ src/model/User.java
@@ -15,6 +15,64 @@ public class User implements Serializable{
 	ArrayList<Album> albums = new ArrayList<>();
 	private String userName;
 	
+	/**
+	 * Constructor for User, takes in userName.
+	 * @param userName
+	 */
+	public User(String userName) {
+		this.userName = userName;
+	}
+	
+	/**
+	 * Returns the Albums that the user has
+	 * @return
+	 */
+	public ArrayList<Album> getAlbums(){
+		return this.albums;
+	}
+	
+	/**
+	 * Adds the Album to the user's album list
+	 * 
+	 * @param alb
+	 */
+	public void addAlbum(Album alb) {
+		this.albums.add(alb);
+	}
+	
+	/**
+	 * Deletes the album from the user's album list
+	 * 
+	 * @param alb
+	 */
+	public void deleteAlbum(Album alb){
+		this.albums.remove(alb);
+	}
+	
+	
+	/**
+	 * Returns true if the user already has an album with the same name
+	 * @param alb
+	 * @return
+	 */
+	public boolean hasAlbum(Album alb) {
+		return albums.contains(alb);
+	}
+	
+	
+	/**
+	 *Returns true if Object is a User and username equals current username
+	 */
+	@Override 
+	public boolean equals(Object o) {
+		if(!(o instanceof User)|| o==null) {
+			return false;
+		}else {
+			User u  = (User) o;
+			return userName.equals(u.userName);
+		}
+	}
+	
 	
 	
 }
