39cf2dc8d410dbbce6e5645702a809f76a59bf87
Alay Shah <alayshah9@icloud.com>
2020-11-16 16:01:19 -0500

    launch

---
+++ src/controller/Photos.java
@@ -0,0 +1,43 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Photos extends Application {
+	
+	
+	private static Stage window;
+	
+	
+	
+	public static void main(String [] args) {
+		launch(args); 
+	}
+
+	@Override
+	public void start(Stage mainStage) throws Exception {
+		// TODO Auto-generated method stub
+		window = mainStage;
+
+		FXMLLoader loader  = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/view/logInScreen.fxml"));
+		
+		Parent root  = loader.load();
+
+		mainStage.setTitle("Photos");
+
+		Scene logIn = new Scene (root);
+
+		mainStage.setResizable(false);
+		mainStage.setScene(logIn);
+		mainStage.show();		
+		
+		
+	}
+	
+	
+
+}
