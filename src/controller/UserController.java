d623be7f2ee01c17ec3976652ed0ca6312383045
Alay Shah <alayshah9@icloud.com>
2020-11-17 16:33:28 -0500

    javaDoc comments added, renameAlbum, openAlbum, search functions added

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,6 +1,10 @@
 package controller;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Optional;
 
 import javafx.collections.FXCollections;
@@ -21,6 +25,8 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.layout.GridPane;
 import model.Album;
+import model.Photo;
+import model.Tag;
 import model.User;
 import model.UserList;
 
@@ -33,7 +39,7 @@ public class UserController {
 	@FXML ChoiceBox<String> choiceBox;
 	
 	@FXML ListView<Album> listView;
-	
+	@FXML TextField searchBar;
 	
 	ObservableList<Album> albums;
 	
@@ -41,18 +47,31 @@ public class UserController {
 	
 	
 	
+	/**
+	 * Shows all the albums that the current user has. Populates the ListView with Albums.
+	 * 
+	 * @param user
+	 * @see User
+	 * @see Album
+	 */
 	public void start(User user) {
 		me = user;
 		choiceBox.setItems(FXCollections.observableArrayList("Tag", "Date"));
 		choiceBox.setValue("Tag");
 		albums = FXCollections.observableArrayList();
-		System.out.println(me+" num Albums"+ me.getAlbums().size());
-		System.out.println(me.getAlbums().get(0));
+		System.out.println(me+" num Albums "+ me.getAlbums().size());
+//		System.out.println(me.getAlbums().get(0));
 		albums.addAll(me.getAlbums());
 		listView.setItems(albums);
 	
 	}
 	
+	/**
+	 * Adds new album to the User's library. Makes sure the user doesn't have another album with the same name and that name is not blank.
+	 * @param e
+	 * @see Album
+	 * @see User
+	 */
 	public void newAlbum(ActionEvent e) {
 		Dialog<String> getNewAlbum = new Dialog<>();
 		getNewAlbum.setTitle("Create New Album");
@@ -100,10 +119,63 @@ public class UserController {
 		
 	}
 	
+	/**
+	 * Renames the selected album.
+	 * @param e
+	 */
 	public void renameAlbum(ActionEvent e) {
+		Album alb =  listView.getSelectionModel().getSelectedItem();
+		Dialog<String> rename = new Dialog<>();
+		rename.setTitle("Rename Album");
+		ButtonType add = new ButtonType("Rename", ButtonData.OK_DONE);
+		rename.getDialogPane().getButtonTypes().addAll(add, ButtonType.CANCEL);
+		GridPane gridPane = new GridPane();
+	    gridPane.setHgap(10);
+	    gridPane.setVgap(10);
+	    gridPane.setPadding(new Insets(20, 150, 10, 10));
+	    TextField userInput = new TextField();
+	    userInput.setPromptText("Enter Album Name");
+	    gridPane.add(userInput, 1, 0);
+		rename.getDialogPane().setContent(gridPane);
+		rename.setResultConverter(dialogButton->{
+			if(dialogButton==add) {
+				return userInput.getText();
+			}
+			return null;
+		});
+		Optional<String> result = rename.showAndWait();
+		result.ifPresent(name -> {
+			name = name.trim();
+			if(me.hasAlbum(new Album(name))){
+				Alert a = new Alert(AlertType.ERROR);
+				a.setHeaderText("Album Already Exists");
+				a.showAndWait();
+				return;
+			}
+			if(name.isBlank()) {
+				Alert a = new Alert(AlertType.ERROR);
+				a.setHeaderText("Enter Valid Album Name");
+				a.showAndWait();
+				return;
+			}
+			alb.rename(name);
+			try {
+				UserList.getUserList().writeApp();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+		});
+		start(me);
+		
 		
 	}
 	
+	/**
+	 * Deletes selected album
+	 * @param e
+	 */
 	public void deleteAlbum(ActionEvent e) {
 		Album alb = listView.getSelectionModel().getSelectedItem();
 		Alert a = new Alert(AlertType.CONFIRMATION);
@@ -125,13 +197,160 @@ public class UserController {
 		
 	}
 	
-	public void openAlbum(ActionEvent e) {
+	/**
+	 * Opens selected Album in a new View.
+	 * @param e
+	 * @throws IOException 
+	 */
+	public void openAlbum(ActionEvent e) throws IOException {
+		
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/PhotosListView.fxml"));
+		Parent root = (Parent) loader.load();
+		Scene user  = new Scene(root);
+		AlbumController next = loader.getController();
+		next.start(listView.getSelectionModel().getSelectedItem(),me, false);
+		Photos.window.setScene(user);
+	}
+	
+	/**
+	 * Opens view with all photos that are part of the search hit.
+	 * @param e
+	 * @throws IOException 
+	 */
+	public void searchHit(ActionEvent e) throws IOException {
+		ArrayList<Photo> searchResult = new ArrayList<>();
+		String query = searchBar.getText().trim();
+		String [] queries = query.split(" ");
+		if(choiceBox.getValue().equals("Tag")) {
+			
+			String [] q1;
+			String []q2;
+			
+			boolean isAnd;
+			if(queries.length!=1&&queries.length!=3) {
+				Alert a  = new Alert(AlertType.ERROR);
+				a.setContentText("Searches should be of format: Tag=value (AND/OR Tag=value)");
+				a.showAndWait();
+				return;
+			}
+			if(queries.length==1) {
+				q1 = queries[0].split("=");
+				if(q1.length!=2) {
+					Alert a  = new Alert(AlertType.ERROR);
+					a.setContentText("Searches should be of format: Tag=value (AND/OR Tag=value)");
+					a.showAndWait();
+					return;
+				}else {
+					//search
+					Tag t = new Tag(q1[0],q1[1]);
+					for(Album alb: me.getAlbums()) {
+						for(Photo p: alb.searchTag(t)) {
+							if(!searchResult.contains(p)) {
+								searchResult.add(p);
+							}
+						}
+					}
+				}
+			} else if(queries[1].toUpperCase().equals("AND")|| queries[1].toUpperCase().equals("OR")) {
+				if(queries[1].length()==3) {
+					isAnd=true;
+				}else isAnd = false;
+				
+				q1 = queries[0].split("=");
+				q2 = queries[2].split("=");
+				
+				if(q1.length==2 && q2.length==2) {
+					Tag t1 = new Tag(q1[0], q1[1]);
+					Tag t2 = new Tag(q2[0], q2[1]);
+					for(Album alb: me.getAlbums()) {
+						for(Photo p: alb.searchTag(t1, t2, isAnd)) {
+							if(!searchResult.contains(p)) {
+								searchResult.add(p);
+							}
+						}
+					}
+				}else {
+					Alert a  = new Alert(AlertType.ERROR);
+					a.setContentText("Searches should be of format: Tag=value (AND/OR Tag=value)");
+					a.showAndWait();
+					return;
+				}
+			}else {
+				Alert a  = new Alert(AlertType.ERROR);
+				a.setContentText("Searches should be of format: Tag=value (AND/OR Tag=value)");
+				a.showAndWait();
+				return;
+			}
+			
+		}else {
+			//search by date
+			if(queries.length!=3||!(queries[1].equals("to"))) {
+				Alert a  = new Alert(AlertType.ERROR);
+				a.setContentText("Searches should be of format: mm/dd/yyy to mm/dd/yyyy");
+				a.showAndWait();
+				return;
+			}
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+			Date min = new Date();
+			Date max = new Date();
+			try {
+				min = sdf.parse(queries[0]);
+				max = sdf.parse(queries[2]);
+			}catch(ParseException e1) {
+				//alert
+				Alert a  = new Alert(AlertType.ERROR);
+				a.setContentText("Searches should be of format: mm/dd/yyy to mm/dd/yyyy");
+				a.showAndWait();
+				return;
+				
+			}
+			
+			if(! sdf.format(min).equals(queries[0])|| ! sdf.format(max).equals(queries[2])) {
+				//alert
+				Alert a  = new Alert(AlertType.ERROR);
+				a.setContentText("Searches should be of format: mm/dd/yyy to mm/dd/yyyy");
+				a.showAndWait();
+				return;
+			}else {
+				//search
+				for(Album album: me.getAlbums()) {
+					for(Photo p: album.searchDate(min, max)) {
+						if(!searchResult.contains(p)) {
+							searchResult.add(p);
+						}
+					}
+				}
+			}
+			
+		}
+		if (searchResult.size()==0) {
+			//alert no matches
+			Alert a  = new Alert(AlertType.ERROR);
+			a.setContentText("No matches");
+			a.showAndWait();
+			return;
+		}
+		
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/PhotosListView.fxml"));
+		Parent root = (Parent) loader.load();
+		Scene user  = new Scene(root);
+		AlbumController next = loader.getController();
+		next.start(new Album("temp",searchResult),me, true);
+		Photos.window.setScene(user);
+		
+		
+		
+		
 		
 	}
 	
-	public void searchHit(ActionEvent e) {
-		
-	}
+	
+	
+	/**
+	 * Returns to logINscreen
+	 * @param e
+	 */
 	public void logOut(ActionEvent e) {
 		
 		Alert confirm = new Alert(AlertType.CONFIRMATION);
